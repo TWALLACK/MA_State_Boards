4fd51432d091f480efde76481689366ce4a39914
TWALLACK <TWALLACK@GMAIL.COM>
2014-12-28 22:19:41 -0500

    Create GrabLists.py
    This script grabs a copy of every list of board members on the Massachusetts boards and commissions web site. http://appointments.state.ma.us/

---
+++ GrabLists.py
@@ -0,0 +1,45 @@
+#This script finds and downloads every board list on the Massachusetts governor's boards and commissions web site
+
+#import modules
+import urllib2
+import re
+
+#URLs for all the sections of the state's boards and commissions site
+URL=["http://appointments.state.ma.us/Results.aspx?PolArea=A%26F",
+"http://appointments.state.ma.us/Results.aspx?PolArea=EDU",
+"http://appointments.state.ma.us/Results.aspx?PolArea=ENV",
+"http://appointments.state.ma.us/Results.aspx?PolArea=OHS",
+"http://appointments.state.ma.us/Results.aspx?PolArea=C%26D",
+"http://appointments.state.ma.us/Results.aspx?PolArea=LAB",
+"http://appointments.state.ma.us/Results.aspx?PolArea=MISC",
+"http://appointments.state.ma.us/Results.aspx?PolArea=DPL",
+"http://appointments.state.ma.us/Results.aspx?PolArea=EPS",
+"http://appointments.state.ma.us/Results.aspx?PolArea=T%26C"]
+
+#Initialize global variables
+strings =[]
+boards = []
+
+#Find the URLs for each individual board lists
+for page in range(len(URL)):
+    web_request = urllib2.urlopen(URL[page])
+    html = web_request.read()
+    strings = re.findall(r'brdid=\d+',html)
+    for i in range(len(strings)):
+        boards.append(strings[i])
+print "There are " + str(len(strings)) + " board lists. They are: "
+#print strings
+
+
+#Save each board list to file directory
+for board in range(len(boards)):
+	web_request = urllib2.urlopen("http://appointments.state.ma.us/BoardDetail.aspx?" + (boards[board]))
+	html_board = web_request.read()
+	outfilename = (boards[board])
+	outpath = 'c:/python27/boards3/' #You might want to rename directory
+	outfile = open(outpath+outfilename+".html", 'w')
+	outfile.write(html_board)
+	outfile.close()
+	print "printing" + boards[board]
+print "Done"
+
